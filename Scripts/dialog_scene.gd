82fc61cb6b7d044e2aef0ed7cdcfda905fe903f3
HeHHeyboi <thanathatwave@gmail.com>
2025-05-07 22:22:37 +0700

    Now when click choice emit button.text to Dialog_Scene

--- Scripts/dialog_scene.gd
+++ Scripts/dialog_scene.gd
@@ -1,22 +1,42 @@
 extends Control
 
 @export var DialogList: Array[String]
-var dialog_index: int = 0
 @export var TextBox: Label
 @export var NameBox: Label
 @export var ChoiceContainer: VBoxContainer
-var select_Choice: String
+
 enum DialogType { Dialog, Choice, SelectChoice }
+
+const DIALOG = "dialog"
+const CHOICE = "choice"
+var dialog_index: int = 0
+var select_index = 0
+var select_Choice: String
+var ChoiceDict := {}
+var choiceButton = preload("res://Scene/DialogChoice.tscn")
 
 
 func _ready():
 	show_text(DialogList[dialog_index])
 
 
-# เอาไว้เรียกจาก Scene อื่นๆ
-# อันนี้น่าจะแก้เป็นชื่อไฟล์ text
+# will change to file name?
 func show_dialog(list: Array[String]):
 	pass
+
+
+func read_file(list: Array[String]):
+	for i in list:
+		var split = i.split(":")
+		var header = split.get(0).to_lower()
+		var body = split.get(1)
+		match header:
+			DIALOG, CHOICE:
+				DialogList.append(i)
+			_:
+				if !ChoiceDict.has(header):
+					ChoiceDict[header] = [] as Array[String]
+				ChoiceDict[header].append(body)
 
 
 # TODO:
@@ -37,14 +57,16 @@ func show_text(text: String):
 func parse_text(text: String):
 	var p = text.split(":")
 	var header = p.get(0)
-	var body := p.get(1).split(",")
+	var body = p.get(1).split(",")
+	for i in range(len(body)):
+		body[i] = body.get(i).trim_prefix(" ")
 	match header:
 		"Dialog":
 			return {"type": DialogType.Dialog, "name": body[0], "dialog": body[1]}
 		"Choice":
 			for choice in body:
-				var button = Button.new()
-				button.connect("pressed", click_choice)
+				var button = choiceButton.instantiate()
+				button.connect("get_choice", click_choice)
 				button.text = choice
 				ChoiceContainer.add_child(button)
 			return {"type": DialogType.Choice}
@@ -63,10 +85,12 @@ func next_text() -> void:
 		show_text(DialogList[dialog_index])
 
 
-func click_choice() -> void:
+func click_choice(button: Button) -> void:
+	select_Choice = button.text
+	print(select_Choice)
 	ChoiceContainer.visible = false
-	dialog_index += 1
-	if dialog_index >= len(DialogList):
-		self.visible = false
-	else:
-		show_text(DialogList[dialog_index])
+	# dialog_index += 1
+	# if dialog_index >= len(DialogList):
+	# 	self.visible = false
+	# else:
+	# 	show_text(DialogList[dialog_index])
