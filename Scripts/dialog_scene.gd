9c8baa1f1d53d8bc6056327fcc3b8005b5bc4cca
HeHHeyboi <thanathatwave@gmail.com>
2025-05-08 17:00:23 +0700

    Conflict merge

--- Scripts/dialog_scene.gd
+++ Scripts/dialog_scene.gd
@@ -22,6 +22,7 @@ var MainDialog: Array[String]
 func _ready():
 	read_file(TXT_PATH + test_file)
 	show_text(MainDialog[dialog_stack[-1]])
+	print("Main Dialog size: ", len(MainDialog))
 
 
 # NOTE: อันนี้เอาไว้ใช้จริง จะเรียกผ่าน Object อื่น
@@ -32,11 +33,13 @@ func show_dialog(file_name: StringName):
 func read_file(file_name: StringName):
 	var file = FileAccess.open(file_name, FileAccess.READ)
 	var type = "dialog"
+
 	while !file.eof_reached():
 		var line = file.get_line()
 		line = line.rstrip(" ")
 		if line.findn("#") == 0:
 			continue
+
 		var split = line.split(":")
 		var header = split.get(0).to_lower()
 		match header:
@@ -53,6 +56,7 @@ func read_file(file_name: StringName):
 					ChoiceDict[header] = []
 					type = header
 					continue
+
 	print(MainDialog)
 	print(ChoiceDict)
 	file.close()
@@ -66,6 +70,7 @@ func read_file(file_name: StringName):
 func show_text(text: String):
 	var parse = parse_text(text)
 	DialogButton.disabled = false
+
 	match parse["type"]:
 		DialogType.Dialog:
 			NameBox.text = parse["name"]
@@ -79,8 +84,10 @@ func parse_text(text: String):
 	var p = text.split(":")
 	var header = p.get(0)
 	var body = p.get(1).split(",")
+
 	for i in range(len(body)):
 		body[i] = body.get(i).lstrip(" ")
+
 	match header:
 		"Dialog":
 			return {"type": DialogType.Dialog, "name": body[0], "dialog": body[1]}
@@ -91,22 +98,21 @@ func parse_text(text: String):
 				button.text = choice
 				ChoiceContainer.add_child(button)
 			return {"type": DialogType.Choice}
-		select_Choice:
-			pass
 
 
 func next_text() -> void:
 	dialog_stack[-1] += 1
+	print(dialog_stack)
 	var current_dialog = MainDialog
 	var has_selectChoice = ChoiceDict.has(select_Choice)
 	if has_selectChoice:
 		current_dialog = ChoiceDict[select_Choice]
+
 	if dialog_stack[-1] >= len(current_dialog):
 		if len(dialog_stack) > 1:
 			dialog_stack.pop_back()
 			select_Choice = ""
 			current_dialog = MainDialog
-			dialog_stack[-1] += 1
 			show_text(current_dialog[dialog_stack[-1]])
 		else:
 			self.visible = false
@@ -117,7 +123,10 @@ func next_text() -> void:
 func click_choice(button: Button) -> void:
 	select_Choice = button.text
 	print(select_Choice)
+
+	dialog_stack[-1] += 1
 	dialog_stack.append(0)
+
 	show_text(ChoiceDict[select_Choice][dialog_stack[-1]])
 	ChoiceContainer.visible = false
 	# dialog_index += 1
