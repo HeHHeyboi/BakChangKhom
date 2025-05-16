9e2f9d590ba4dd8a4f3c81f5c850830361947b78
HeHHeyboi <thanathatwave@gmail.com>
2025-05-16 15:17:18 +0700

    Adjust code a bit

--- Scripts/dialog_choice.gd
+++ Scripts/dialog_choice.gd
@@ -1,9 +1,12 @@
 extends Button
 signal get_choice(button: Button)
 
+# func _gui_input(event: InputEvent) -> void:
+# 	if event is InputEventMouseButton:
+# 		event = event as InputEventMouseButton
+# 		if event.get_button_index() == MOUSE_BUTTON_LEFT and event.is_pressed():
+# 			emit_signal("get_choice", self)
 
-func _gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		event = event as InputEventMouseButton
-		if event.get_button_index() == MOUSE_BUTTON_LEFT and event.is_pressed():
-			emit_signal("get_choice", self)
+
+func _on_pressed() -> void:
+	emit_signal("get_choice", self)
