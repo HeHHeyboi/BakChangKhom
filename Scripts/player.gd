fdecfc0b709c4bf0dd5f92444f0c362c1c8b963d
HeHHeyboi <thanathatwave@gmail.com>
2025-07-03 13:20:50 +0700

    Test player sprite

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,7 @@ class_name Player extends CharacterBody2D
 
 const speed = 300
 
+@onready var sprite = $"Idle"
 var Dir = Vector2(0, 0)
 var isDialogShow = false
 
@@ -22,6 +23,12 @@ func _physics_process(_delta: float) -> void:
 		Dir.x -= 1
 	elif Input.is_action_pressed("right", true):
 		Dir.x += 1
+
+	print(bool(-1))
+	if Dir.x < 0:
+		sprite.flip_h = true
+	elif Dir.x > 0:
+		sprite.flip_h = false
 
 	var Vspeed = Dir.normalized() * speed
 	# Vspeed *= abs(Vspeed.normalized())
